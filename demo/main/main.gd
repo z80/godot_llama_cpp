6c25216af109c80cbeddf930212d0590bfdeb838
me <me@me.me>
2024-02-19 12:37:56 -0800

    Test main scene.

--- demo/main/main.gd
+++ demo/main/main.gd
@@ -3,7 +3,9 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var model: Llam
+	var model: LlamaCpp = LlamaCpp.new()
+	var opened_ok: bool = model.load_model( "models/ggml-model-Q8_0.gguf" )
+	print( opened_ok )
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
